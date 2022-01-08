c9e9a1729e03672a4285b8625184385fcdb9e372
Sheeva Kavousian <sheevakavous@gmail.com>
2022-01-08 15:02:17 +0000

    Add component files

---
+++ src/index.js
@@ -0,0 +1,12 @@
+// separate components for 
+//info(photo, name buttons, etc.)
+//about 
+//intrests
+//footer (icons)
+
+import React from "react"
+import ReactDOM from "react-dom"
+import App from "./App"
+
+
+ReactDOM.render(<App />, document.getElementById("root"));
